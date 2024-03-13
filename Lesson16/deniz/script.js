32b27b821e7ec501aa6669c6547776c1e2dd85ad
denislavninov <deniztopcuoglu@gmail.com>
2024-03-14 00:54:41 +0100

    add lesson16 exercises

--- Lesson16/deniz/script.js
+++ Lesson16/deniz/script.js
@@ -34,8 +34,11 @@ function compareNumbers(firstNumber, secondNumber) {
 		return "The first number is equal to the second number.";
 	}
 }
-let result = compareNumbers(firstNumber, secondNumber);
-console.log("exercise 3", compareNumbers(1, 2));
+
+console.log("exercise 3", compareNumbers(1, 3));
+console.log("exercise 3", compareNumbers(2, 1));
+console.log("exercise 3", compareNumbers(4, 4));
+
 
 // 4. Write a function that takes in a string and checks if the first letter is uppercase using if else statement.
 function isFirstLetterUppercase(stringToCheck){
@@ -49,10 +52,41 @@ const firstLetter = stringToCheck[0];
 }
 console.log(isFirstLetterUppercase("Televiision"));
 // 5. Write a function that takes in a number and checks if it's positive, negative, or zero using if else statement.
+function checkNumber(num) {
+    if (num > 0) {
+        return "Positive";
+    } else if (num < 0) {
+        return "Negative";
+    } else {
+        return "Zero";
+    }
+}
+console.log("exercise 4:", checkNumber(5));
+console.log("exercise 4:", checkNumber(-2));
+console.log("exercise 4:", checkNumber(0));
 
 // 6. Write a function that takes in a year and checks if it's a leap year (is divisible by 4 and not divisible by 100 OR divisible by 400) or not using if else statement.
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log("exercise 6:", isLeapYear(2024));
+console.log("exercise 6:", isLeapYear(2025));
+
 
 // 7. Write a function that takes in a temperature and checks if it's above or below freezing using if else statement.
+function isAboveOrBelow(temperature) {
+	if (temperature < 0) {
+		return "Its frozen";
+	} else {
+		return "Its NOT frozen";
+	}
+}
+console.log("exercise 7:", isAboveOrBelow(-10));
+console.log("exercise 7:", isAboveOrBelow(10));
 
 // 8. Write a function that takes in a person's height and weight and checks if they are considered underweight, normal, overweight, or obese using if else statement.
 // Body Mass Index (BMI) is a person’s weight in kilograms (or pounds) divided by the square of height in meters (or feet). A high BMI can indicate high body fatness.
@@ -63,8 +97,36 @@ console.log(isFirstLetterUppercase("Televiision"));
 // 30.0 and Above	Obesity
 
 // 9. Write a function that takes in a person's age and checks if they are a child (age 0-12), teenager (age 13-19), adult (age 20-64), or senior citizen (age 65 and above) using if else statement.
+function checkingAge(age) {
+	if (age <= 12) {
+		return "Child";
+	} else if (age >= 13 && age <= 19) {
+		return "Teenager";
+	} else if (age >= 20 && age <= 64) {
+		return "Adult";
+	} else if (age >= 65) {
+		return "Senior Citizen";
+	}
+}
+console.log("exercise 9:", checkingAge(12));
+console.log("exercise 9:", checkingAge(20));
 
 // 10. Write a function that takes age as a number and whether they are a student (yes or no). If the age is less than 18 and the user is a student, print "You are a student and not old enough to vote". If the age is between 18 and 65 (inclusive) and the user is not a student, print "You are eligible to vote". If the age is greater than 65, print "You are a senior citizen". If the user is a student but their age is greater than or equal to 18, print "You are old enough to vote".
+function checkVoting(age, isStudent) {
+	if (age < 18 && isStudent === "yes") {
+		return "You are a student and not old enough to vote";
+	} else if (age >= 18 && isStudent === "no") {
+		return "You are eligible to vote"; 
+	} else if (age > 65) {
+		return "You are a senior citizen";
+	} else if (isStudent === "yes" && age >= 18) {
+		return "You are old enough to vote";
+	} 
+}
+console.log("exercise 10:", checkVoting(17, "yes"));
+console.log("exercise 10:", checkVoting(20, "no"));
+console.log("exercise 10:", checkVoting(18, "yes"));
+
 
 // 11. Write a function that asks the user to enter their salary and their years of experience. If the salary is less than $30,000 and the years of experience are less than 5, print "Sorry, we cannot offer you the job at this time". If the salary is less than $50,000 and the years of experience are between 5 and 10 (inclusive), print "We can offer you the job at a lower salary". If the salary is greater than or equal to $50,000 or the years of experience are greater than or equal to 10, print "We can offer you the job at the requested salary".
 
