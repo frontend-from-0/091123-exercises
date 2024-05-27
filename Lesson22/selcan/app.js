4ded4aed17b7254c011c244cf6756fef53311fe9
selceli <145831296+selceli@users.noreply.github.com>
2024-05-27 13:49:39 +0300

    Selcan-lesson22 (#70)
    * solution1
    
    * solution2
    
    * lesson22-html
    
    * lesson22-css
    
    * solution3
    
    * solution4
    
    * html2
    
    * solution5

---
+++ Lesson22/selcan/app.js
@@ -0,0 +1,35 @@
+const formElement = document.getElementById('toDoForm');
+const formInput = document.getElementById('toDoInput');
+const list = document.getElementById('toDoList');
+
+formElement.addEventListener('submit', function (event) {
+	// By default, the page is reloded when form is submitted. To avoid it, we need to add `preventDetault()`;
+	event.preventDefault();
+
+  // TODO: check that input value has correct format (e.g. minimum 3 chars, no extra spaces)
+	const listItem = document.createElement('li');
+	const button = document.createElement('button');
+	button.innerText = 'x';
+	button.classList.add('button--light');
+	const span = document.createElement('span');
+	if(inputValue.length < 3){
+	const inputValue = formInput.value.trim();
+	alert('Please enter at least 3 characters.');
+    return;
+}   
+    span.innerText = formInput.value;
+    button.addEventListener('click', function () {
+		listItem.remove();
+	});
+  // TODO: Add event listener for click event. When click happens, we need to toggle 'completed' class on the span
+	listItem.append(span);
+	listItem.append(button);
+	list.append(listItem);
+	  // TODO: Clear input element after everything is done in this function
+    span.addEventListener(click,function(){
+	span.classList.toggle('completed');	
+})
+formInput.value = "";
+});
+  
+
