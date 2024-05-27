678fe23e3761e0afe2a0f9461f2357fc594f82db
fyzaden <feyza.gazioglu8@gmail.com>
2024-05-28 01:04:33 +0300

    Lesson16-feyza

--- Lesson16/feyza/script.js
+++ Lesson16/feyza/script.js
@@ -30,39 +30,39 @@ console.log('Exercise 2', canVote(10), canVote(30), canVote(75));
 // 3. Write a function that asks the user to enter two numbers and checks if the first number is greater than, less than, or equal to 
 //the second number using if else statement.
 
-function isFirstNumber(number1, number2){
-	if(number1 > number2){
+function isFirstNumber(number1, number2) {
+	if (number1 > number2) {
 		console.log("Exercise 3", number1, number2, "is greater than.");
-	} else if(number1 === number2){
+	} else if (number1 === number2) {
 		console.log("Exercise 3", number1, number2, "is equal.");
-	}else{
+	} else {
 		console.log("Exercise 3", number1, number2, "is less than.");
 	}
 }
-console.log("Exercise 3", isFirstNumber(8,95));
+console.log("Exercise 3", isFirstNumber(8, 95));
 
 // 4. Write a function that takes in a string and checks if the first letter is uppercase using if else statement.
 
-function isFirstLetterUppercase(stringToCheck){
-const firstLetter = stringToCheck[0];
-  if (firstLetter === firstLetter.toUpperCase()) {
-    return true;
-  } else {
-    return false;
-  }
+function isFirstLetterUppercase(stringToCheck) {
+	const firstLetter = stringToCheck[0];
+	if (firstLetter === firstLetter.toUpperCase()) {
+		return true;
+	} else {
+		return false;
+	}
 
 }
 console.log(isFirstLetterUppercase("Television"));
 
 // 5. Write a function that takes in a number and checks if it's positive, negative, or zero using if else statement.
 
-function isPositive(number){
-	if(number > 0) {
+function isPositive(number) {
+	if (number > 0) {
 		return "positive";
-	} else if(number === 0){ 
-	    return "zero";
-	} else{
-        return "negative";
+	} else if (number === 0) {
+		return "zero";
+	} else {
+		return "negative";
 	}
 }
 console.log("Exercise 5", isPositive(29));
@@ -70,10 +70,10 @@ console.log("Exercise 5", isPositive(29));
 // 6. Write a function that takes in a year and checks if it's a leap year (is divisible by 4 and not divisible by 100 OR divisible by 400)
 // or not using if else statement.
 
-function isYears(leapYear){
-	if(leapYear % 4 === 0 && leapYear % 100 !== 0 && leapYear % 400 !== 0){
+function isYears(leapYear) {
+	if (leapYear % 4 === 0 && leapYear % 100 !== 0 && leapYear % 400 !== 0) {
 		return "It is a leap year";
-	} else{
+	} else {
 		return "It is not a leap year";
 	}
 }
@@ -81,14 +81,14 @@ console.log("Exercise 6", isYears(2035));
 
 // 7. Write a function that takes in a temperature and checks if it's above or below freezing using if else statement.
 
-function isTemperature(degree){
-	if(degree > 0){
+function isTemperature(degree) {
+	if (degree > 0) {
 		return "Temperature is above freezing";
-	}else if(degree < 0){
+	} else if (degree < 0) {
 		return "Temparature is below freezing";
-	} 
+	}
 }
-console.log("Exercise 7",isTemperature(-18));
+console.log("Exercise 7", isTemperature(-18));
 
 // 8. Write a function that takes in a person's height and weight and checks if they are considered underweight, normal, overweight, or obese using if else statement.
 // Body Mass Index (BMI) is a person’s weight in kilograms (or pounds) divided by the square of height in meters (or feet). A high BMI can indicate high body fatness.
@@ -99,33 +99,33 @@ console.log("Exercise 7",isTemperature(-18));
 // 30.0 and Above	Obesity
 
 
-function BMI(height,weight){
-let result = weight / height * 2;
-   if(result < 18.5){
-	console.log("Exercise 8", "Underweight", + result);
-   } else if(result >= 18.5 && result <= 24.9){
-	console.log("Exercise 8", "Healty Weight", + result);
-   } else if(result >= 25 && result <= 29.9){
-	console.log("Exercise 8", "Overweight", + result);
-   } else if(result >= 30){
-	console.log("Exercise 8", "Obesity", + result);
-   }
+function BMI(height, weight) {
+	let result = weight / height * 2;
+	if (result < 18.5) {
+		console.log("Exercise 8", "Underweight", + result);
+	} else if (result >= 18.5 && result <= 24.9) {
+		console.log("Exercise 8", "Healty Weight", + result);
+	} else if (result >= 25 && result <= 29.9) {
+		console.log("Exercise 8", "Overweight", + result);
+	} else if (result >= 30) {
+		console.log("Exercise 8", "Obesity", + result);
+	}
 }
-console.log(BMI(55,1.58));
+console.log(BMI(55, 1.58));
 
 // 9. Write a function that takes in a person's age and checks if they are a child (age 0-12), teenager (age 13-19), adult (age 20-64),
 // or senior citizen (age 65 and above) using if else statement.
 
-function isPersonAge(age){
-	if(age > 0 && age <= 12){  
-	return "Child";
-} else if (age >= 13 && age <= 19){
-    return "Teenager";
-} else if(age >= 20 && age <= 64){
-	return "Adult";
-} else if(age >= 65){
-    return "Senior Citizen";
-}
+function isPersonAge(age) {
+	if (age > 0 && age <= 12) {
+		return "Child";
+	} else if (age >= 13 && age <= 19) {
+		return "Teenager";
+	} else if (age >= 20 && age <= 64) {
+		return "Adult";
+	} else if (age >= 65) {
+		return "Senior Citizen";
+	}
 }
 console.log("Exercise 9", isPersonAge(28));
 
@@ -135,51 +135,50 @@ console.log("Exercise 9", isPersonAge(28));
 // If the user is a student but their age is greater than or equal to 18, print "You are old enough to vote".
 
 
-function canVote(age){
-	let person = student;
-	if(age < 18 && person){
+function canVote(age, isStudent) {
+	if (age < 18 && isStudent) {
 		return "You are a student and not old enough to vote";
-	}else if(age >= 18 && age <=65 && !person){ 
-	    return "You are eligible to vote";
-	}else if(age >=65){
+	} else if (age >= 18 && age <= 65 && !isStudent) {
+		return "You are eligible to vote";
+	} else if (age >= 65) {
 		return "You are a senior citizen";
-	}else if(age > 18 && person){
+	} else if (age > 18 && isStudent) {
 		return "You are old enough to vote";
 	}
 }
-console.log("Exercise 10", canVote(49));
+console.log("Exercise 10", canVote(49, false));
 
 // 11. Write a function that asks the user to enter their salary and their years of experience. If the salary is less than $30,000 and the years of experience
 // are less than 5, print "Sorry, we cannot offer you the job at this time". If the salary is less than $50,000 and the years of experience are 
 //between 5 and 10 (inclusive), print "We can offer you the job at a lower salary". If the salary is greater than or equal to $50,000 or the years of 
 //experience are greater than or equal to 10, print "We can offer you the job at the requested salary".
 
-function jobOffer(salary,experience){
-	if(salary < 30000 && experience <5){
+function jobOffer(salary, experience) {
+	if (salary < 30000 && experience < 5) {
 		print("Sorry, we cannot offer you the job at this time");
-	}else if(salary < 50000 && experience > 5 && experience < 10){
+	} else if (salary < 50000 && experience > 5 && experience < 10) {
 		print("We can offer you the job at a lower salary");
-	}else if(salary >= 50000 && experience >= 10){
+	} else if (salary >= 50000 && experience >= 10) {
 		print("We can offer you the job at the requested salary");
 	}
 }
 
-console.log("Exercise 11",jobOffer(48000,7));
+console.log("Exercise 11", jobOffer(48000, 7));
 
 // 12. Write a function that asks the user to enter their score on a test. If the score is less than 60, print "You failed the test". 
 //If the score is between 60 and 70 (inclusive), print "You passed the test, but your grade is not great". If the score is between 71 and 80 (inclusive),
 // print "You got a B". If the score is between 81 and 90 (inclusive), print "You got an A". If the score is greater than 90, print "You got an A+ - Great job!".
 
-function isScoreTest(score){
-	if(score < 60){
+function isScoreTest(score) {
+	if (score < 60) {
 		print("You failed the test");
-	}else if(score <= 60 && score >= 70){
+	} else if (score <= 60 && score >= 70) {
 		print("You passed the test, but your grade is not great");
-	}else if(score <= 71 && score >= 80){
+	} else if (score <= 71 && score >= 80) {
 		print("You got a B");
-	}else if(score <=81 && score >= 90){
+	} else if (score <= 81 && score >= 90) {
 		print("You got an A");
-	}else if(score > 90){
+	} else if (score > 90) {
 		print("You got an A+ - Great job!");
 	}
 }
@@ -190,11 +189,11 @@ console.log("Exercise 12", isScoreTest(100));
 
 function getSeasonName(numberOfAMonth) {
 	switch (numberOfAMonth) {
-      // numberOfAMonth === 12
+		// numberOfAMonth === 12
 		case 12:
 		case 1:
 		case 2:
-      // console.log('Winter');
+			// console.log('Winter');
 			// break;
 			return 'Winter';
 		case 3:
@@ -209,8 +208,8 @@ function getSeasonName(numberOfAMonth) {
 		case 10:
 		case 11:
 			return 'Autumn';
-    default: 
-      return 'Value is not supported, please enter value between 1 and 12.';
+		default:
+			return 'Value is not supported, please enter value between 1 and 12.';
 	}
 }
 
@@ -218,19 +217,19 @@ console.log('Exercise 13', getSeasonName(1), getSeasonName(5), getSeasonName(20)
 
 // 14. Write a function that asks the user to enter a grade letter (A, B, C, D, or F) and displays a message indicating whether the grade is passing (A, B, C) or 
 //failing (D, F). Use a switch statement to handle the different cases.
-function gradeLetter(letter){
-	letter=letter.toUpperCase();
+function gradeLetter(letter) {
+	letter = letter.toUpperCase();
 	switch (letter) {
 		case A:
 		case B:
 		case C:
 			return "You passed :)";
 			break;
-        case D:
+		case D:
 		case F:
 			return "You failed :(";
 			break;
-        default:
+		default:
 			return "Value is not supported";
 	}
 }
@@ -239,12 +238,13 @@ console.log("Exercise 14", gradeLetter(A));
 // 15. Write a function that asks the user to enter their age, and then calculates and displays their age in dog years (where one human year is equivalent to
 // seven dog years). If the user enters a negative number or zero, display an error message.
 
-function calculatesDogYears(age){
-	let dogYears = age * 7;
-	if(age <=0){
-		prompt("Error: Age must be a positive number.");
-	} else{
-		prompt("Your age in dog years is ${dogYears}.");
+function calculatesDogYears(age) {
+	if (age <= 0) {
+		let dogYears = age * 7;
+		return dogYears;
+
+	} else {
+		return "Your age in dog years is ${dogYears}.";
 	}
 }
 console.log("Exercise 15", calculatesDogYears(28));
