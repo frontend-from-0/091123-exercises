114aa7b909787c3a8d4040248fe7a9a485348106
denislavninov <deniztopcuoglu@gmail.com>
2024-06-09 22:47:35 +0200

    add changes lesson 24deniz

--- Lesson24/deniz/newsletter assignment/index.js
+++ Lesson24/deniz/newsletter assignment/index.js
@@ -2,8 +2,20 @@ const form = document.querySelector('form');
 const card_1 = document.querySelector('.card');
 const card_2 = document.querySelector('.card-2');
 
-form.addEventListener('submit', function(e) {
+
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    card_1.classList.add('hide');
-    card_2.classList.remove('hide');
+
+    const emailInput = document.getElementById('email');
+    const emailValue = emailInput.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailRegex.test(emailValue)) {
+        card_1.classList.add('hide');
+        card_2.classList.remove('hide');
+    } else {
+        alert('Please enter a valid email address.');
+    }
+
+
 });
