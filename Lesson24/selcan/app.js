eb58953cbd7562194fd110dc3c524654b6adabb9
Selceli <selcanece@gmail.com>
2024-06-21 09:31:18 +0300

    2

--- Lesson24/selcan/app.js
+++ Lesson24/selcan/app.js
@@ -9,7 +9,6 @@ form.addEventListener('submit', function (event) {
 
 	const nameInput = document.getElementById('name');
 	const nameInputError = document.getElementById('nameError');
-
 	if (nameInput.value.length < 3) {
 		nameInputError.textContent = 'Name should be at least 3 characters long.';
 		isFormCorrect = false;
@@ -17,9 +16,19 @@ form.addEventListener('submit', function (event) {
 		nameInputError.textContent = '';
 	}
 
+
+	const surnameInput = document.getElementById("surname");
+	const surnameInputError = document.getElementById("surnameError");
+	if(surnameInput.value.length <3 && surnameInput !== "string"){
+		surnameInputError.textContent ='Surname should be at least 3 characters long and contain only letter.';
+		isFormCorrect = false;
+	} else {
+		surnameInputError.textContent ='';
+	}
+
+
+    const emailInput = document.getElementById('email');
 	const emailInputError = document.getElementById('emailError');
-	const emailInput = document.getElementById('email');
-  
 	if (!emailRegex.test(emailInput.value)) {
 		emailInputError.textContent = 'Please enter correct email address.';
 		isFormCorrect = false;
@@ -27,9 +36,30 @@ form.addEventListener('submit', function (event) {
 		emailInputError.textContent = '';
 	}
 
+	const ageInput = document.getElementById("age");
+	const ageInputError = document.getElementById("ageError");
+	if(ageInput.value <= 0 || ageInput.value > 120){
+		ageInputError.textContent ="Please enter your age between 0-120.";
+		isFormCorrect = false;
+	} else {
+		ageInputError.textContent = '';
+	}
+
+	const phoneInput = document.getElementById("phone");
+	const phoneInputError = document.getElementById("phoneError");
+	if(!phoneRegex.test(phoneInput.value)){
+		phoneInputError.textContent ="Please enter your phone number in true format";
+		isFormCorrect = false;
+	} else {
+		phoneInputError.textContent = '';
+	}
+
 	const formData = {
 		name: nameInput.value,
 		email: emailInput.value,
+		surname : surnameInput.value,
+		age : ageInput.value,
+		phone: phoneInput.value
 	};
 
 	if (isFormCorrect) {
