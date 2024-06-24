27704fce5cd9fbe8c72641315c16a05c248b953f
Selceli <selcanece@gmail.com>
2024-06-24 10:07:45 +0300

    commit1

--- Lesson26/Selcan/app/script.js
+++ Lesson26/Selcan/app/script.js
@@ -1,12 +1,17 @@
 // Register on Unsplash to get your own API key
-const API_KEY = '';
+const API_KEY = 'hQxXexpjCejVZJjkdCKHVTx73C_F4V4vLXkNvgT0oVE';
 
-// TODO: Add a new button in HTML to crear the screen
+// TODO: Add a new button in HTML to clear the screen
 
 const button = document.getElementById('fetch-button');
+const clearButton=document.getElementById("clear-button");
+
 button.addEventListener('click', function(){
   // TODO: Add an input in HTML (e.g. slider) to select how many new picutres should be fetched. Depending on the user input, the count value in the request should be changed to value selected by user
-  fetch(`https://api.unsplash.com/photos/randm?count=10&client_id=${API_KEY}`)
+
+  const imageCount=document.getElementById("image-count").value;
+
+  fetch(`https://api.unsplash.com/photos/random?count=10&client_id=${API_KEY}`)
   .then(response => response.json()) //res or response
   .then(data => {
     if(data !== -1){
@@ -23,5 +28,10 @@ button.addEventListener('click', function(){
       document.getElementById("image-container").append(imageWrapper);
     }) }
   })
-  .catch(error => console.error(error));
+  .catch(error => console.error(error)); 
+  const errorMessage ="Hata:${error.message}";
+  document.getElementById("image-container").append(errorMessage);  
 });
+clearButton.addEventListener("click",function()){
+  document.getElementById("image-container").innerHTML=" ";
+  }
