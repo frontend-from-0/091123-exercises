9fb80c4d0fd7f42cd12ece989cdce9cd53e50406
selceli <145831296+selceli@users.noreply.github.com>
2024-07-13 21:45:33 +0300

    lesson27-Selcan (#92)
    * solution1
    
    * solution2
    
    * allsolution1
    
    * all2
    
    * changes1
    
    * solution2
    
    * solution3
    
    * 1
    
    * commit1
    
    * commit2
    
    * commit3
    
    * commit4
    
    * commit6
    
    * commit7
    
    * commit8
    
    * commit9

---
+++ Lesson27/selcan/create/script.js
@@ -0,0 +1,52 @@
+// URL of the JSONPlaceholder API
+const URL = 'https://jsonplaceholder.typicode.com/posts';
+const formPost = document.getElementById('createPostForm');
+
+formPost.addEventListener('submit', function (event) {
+  event.preventDefault();
+  createPost();
+
+});
+
+isFormCorrect = true;
+
+function createPost() {
+  const titleText = document.getElementById("titleInput").value;
+  const bodyText = document.getElementById("bodyInput").value;
+
+  fetch(URL, {
+    method: "POST",
+    body: JSON.stringify({
+      title: titleText,
+      body: bodyText
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      console.log("Başari ile kaydedildi.");
+
+      const successMessage = "Your post was created succesfully...";
+      document.getElementById("message").textContent = successMessage
+
+      const buttonElement = document.createElement("button");
+      buttonElement.setAttribute("class", "buttonElement");
+      buttonElement.innerText = "Turn to main screen";
+      document.getElementById("post").append(buttonElement);
+
+      buttonElement.addEventListener("click",() =>{
+      window.location.href = `../index.html?`;
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+
+
+};
+
+
---
+++ Lesson27/selcan/update-post/script.js
@@ -0,0 +1,54 @@
+const urlParams = new URLSearchParams(window.location.search);
+const postId = urlParams.get('postId');
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://jsonplaceholder.typicode.com/posts/' + postId)
+    .then((response) => response.json())
+    .then((post) => {
+      document.getElementById('title-input').value = post.title;
+      document.getElementById('body-input').value = post.body;
+    })
+    .catch((error) => console.error('Error:', error));
+});
+
+const formElement = document.getElementById('update-post-form');
+
+formElement.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const postTitleText = document.getElementById('title-input').value;
+  console.log('postTitleText', postTitleText);
+  const postBodyText = document.getElementById('body-input').value;
+  console.log('postBodyText', postBodyText);
+
+  // Valid form input!!!
+
+  fetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: postTitleText,
+      body: postBodyText
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      const successMessage = "Your post was updated succesfully...";
+      document.getElementById("message").textContent = successMessage;
+
+      console.log("Your post was updated successfully...");
+
+      const buttonElement = document.createElement("button");
+      buttonElement.setAttribute("class", "buttonElement");
+      buttonElement.innerText = "Turn to main screen";
+      document.getElementById("post").append(buttonElement);
+
+      buttonElement.addEventListener("click", () => {
+        window.location.href = `../index.html?`;
+
+      });
+    })
+    .catch((error) => console.error('Error:', error));
+});
