d0fcb5d6534dc01d120e193650f6da4cab41bb20
Selceli <selcanece@gmail.com>
2024-09-03 21:04:20 +0300

    add35to34

--- Lesson34/selcan/lesson34-app/selcan-35/lesson34-app/src/App.js
+++ Lesson34/selcan/lesson34-app/selcan-35/lesson34-app/src/App.js
@@ -5,18 +5,19 @@ import { useState } from 'react';
 import { todoData } from './data';
 import { UserProvider } from './userContext';
 import { Account } from './components/Account';
-import './App.css';
 
-//todo:create a new component.
+import './App.css';
+import Login from './components/Form';
+
+// TODO: Create a new Component that contains log in form, so that user can type username and password and click submit button to log in.
+// If user is logged in, show Account component. 
+// If user is logged out, show Log In component.
+
+
 export const App = () => {
 	const [todos, setTodos] = useState(todoData);
 
 
-
-	const loggedInUser = {
-		isLoggedInUser: true,
-		username: "johndoe01"
-	};
 	const loggedOutUser = {
 		isLoggedInUser: false
 	};
@@ -26,6 +27,7 @@ export const App = () => {
 
 			<div className='container'>
 				<Navbar />
+				<Login />
 				<Account />
 				<div className='app'>
 					<List todos={todos} updateTodos={setTodos} />
