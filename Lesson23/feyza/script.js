4f7b526c62f3330998849cec60c964d2b6f12675
fyzaden <feyza.gazioglu8@gmail.com>
2024-10-02 22:26:50 +0300

    add correct Lesson23

--- Lesson23/feyza/script.js
+++ Lesson23/feyza/script.js
@@ -1,47 +1,7 @@
-window.addEventListener("load", function () {
-    const images = document.querySelectorAll(".carousel-container > img");
-    const indicators = document.getElementsByClassName("indicator");
-    console.log(images, indicators);
-    const prevButton = document.getElementById("prev-btn");
-    const nextButton = document.getElementById("next-btn");
+const accordion = document.getElementsByClassName("container");
 
-    let activeSlide = 0;
-//I can't create all the files myself yet. So I did it by watching the lesson video.    
-
-    function showImage() {
-        for (let i = 0; i < images.length; i++) {
-            images[i].classList.remove("active");
-            indicators[i].classList.remove("active");
-        }
-        console.log("active slide: ", activeSlide);
-        images[activeSlide].classList.add("active");
-        indicators[activeSlide].classList.add("active");
-
-    }
-
-    prevButton.addEventListener("click", showPrevImage);
-    nextButton.addEventListener("click", showNextImage);
-   
-    function showNextImage() {
-        if (activeSlide < images.length - 1) {
-          activeSlide++;
-        } else {
-          activeSlide = 0;
-        }
-    
-        showImage();
-      }
-
-
-      function showPrevImage() {
-        if (activeSlide > 0) {
-          activeSlide--;
-        } else {
-          activeSlide = images.length - 1;
-        }
-        showImage();
-      }
-
-      setInterval(showNextImage, 2500);
-    
-});
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
