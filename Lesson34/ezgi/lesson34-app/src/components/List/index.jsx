ddc2fda95ae2ea518828fbe9d6a4b8dd7305dfbc
Ezgi Akyildiz <ezgii.akyildiz@gmail.com>
2024-10-12 08:32:03 +0300

    lesson34-ezgi

---
+++ Lesson34/ezgi/lesson34-app/src/components/List/index.jsx
@@ -0,0 +1,32 @@
+import './styles.css';
+
+export const List = ({todos,updateTodos}) => {
+
+	function handleClick(id) {
+		updateTodos((prevState) =>
+			prevState.map((todo) =>
+				todo.id === id ? { ...todo, completed: !todo.completed } : todo
+			)
+		);
+	}
+
+	function deleteTodo (id) {
+		const newTodo = todos.filter((todo)=> todo.id != id);
+		updateTodos(newTodo);
+	}
+
+
+
+	return (
+		<ul className='todo__list'>
+			{todos.map((item) => (
+				<li onClick={() => handleClick(item.id)} key={item.id}>
+					<span className={item.completed ? 'completed' : ''}>
+						{item.title}
+					</span>
+					<button onClick={() => deleteTodo(item.id)} className={'btn'}>X</button>
+				</li>
+			))}
+		</ul>
+	);
+};
